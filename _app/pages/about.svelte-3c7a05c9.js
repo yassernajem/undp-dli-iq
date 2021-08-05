40c9b3990c8c8b856a17c369b319f1464b762e4a
uf0 <scnepa222@gmail.com>
2021-08-05 15:33:53 +0200

    Updates

---
+++ _app/pages/about.svelte-3c7a05c9.js
@@ -0,0 +1,1 @@
+import{S as s,i as a,s as t,e as o,t as r,c as e,a as u,g as c,d as n,f as l,F as d,G as f}from"../chunks/vendor-56808b61.js";function i(s){let a,t;return{c(){a=o("h1"),t=r("about")},l(s){a=e(s,"H1",{});var o=u(a);t=c(o,"about"),o.forEach(n)},m(s,o){l(s,a,o),d(a,t)},p:f,i:f,o:f,d(s){s&&n(a)}}}class h extends s{constructor(s){super(),a(this,s,null,i,t,{})}}export{h as default};
