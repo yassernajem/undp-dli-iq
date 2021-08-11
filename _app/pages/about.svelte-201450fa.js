0338a412490dab2600b0c5ff693caa5bed75e069
giorgiouboldi <giorgio.uboldi@gmail.com>
2021-08-11 12:38:32 +0300

    Updates

--- _app/pages/about.svelte-201450fa.js
+++
@@ -1,1 +0,0 @@
-import{S as s,i as a,s as t,e as o,t as r,c as e,a as u,g as c,d as n,f as d,F as l,G as f}from"../chunks/vendor-bd331711.js";function i(s){let a,t;return{c(){a=o("h1"),t=r("about")},l(s){a=e(s,"H1",{});var o=u(a);t=c(o,"about"),o.forEach(n)},m(s,o){d(s,a,o),l(a,t)},p:f,i:f,o:f,d(s){s&&n(a)}}}class h extends s{constructor(s){super(),a(this,s,null,i,t,{})}}export{h as default};
