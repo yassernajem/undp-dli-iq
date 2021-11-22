302c456caae4a8a80cb2e1037dc164c98a9d4efb
uf0 <scnepa222@gmail.com>
2021-11-22 16:00:39 +0100

    Updates

---
+++ _app/pages/about.svelte-41336756.js
@@ -0,0 +1,21 @@
+import{S as V,i as j,s as A,e as n,t as E,k as P,c as o,a as c,g as I,d as a,n as D,b as d,f as S,F as s,G as q}from"../chunks/vendor-8b487b9e.js";function y(M){let t,e,u,i,b,f,r,l,h;return{c(){t=n("div"),e=n("div"),u=n("div"),i=n("h6"),b=E("About"),f=P(),r=n("div"),l=n("p"),h=E(`Cras mattis consectetur purus sit amet fermentum. Praesent commodo cursus magna, vel
+				scelerisque nisl consectetur et. Cras mattis consectetur purus sit amet fermentum. Cras
+				justo odio, dapibus ac facilisis in, egestas eget quam. Integer posuere erat a ante
+				venenatis dapibus posuere velit aliquet. Morbi leo risus, porta ac consectetur ac,
+				vestibulum at eros. Nullam id dolor id nibh ultricies vehicula ut id elit. Sed posuere
+				consectetur est at lobortis. Vestibulum id ligula porta felis euismod semper. Maecenas
+				faucibus mollis interdum. Cum sociis natoque penatibus et magnis dis parturient montes,
+				nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis
+				vestibulum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Fusce dapibus,
+				tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet
+				risus.`),this.h()},l(m){t=o(m,"DIV",{class:!0});var p=c(t);e=o(p,"DIV",{class:!0});var v=c(e);u=o(v,"DIV",{class:!0});var g=c(u);i=o(g,"H6",{class:!0});var _=c(i);b=I(_,"About"),_.forEach(a),g.forEach(a),f=D(v),r=o(v,"DIV",{class:!0});var C=c(r);l=o(C,"P",{});var x=c(l);h=I(x,`Cras mattis consectetur purus sit amet fermentum. Praesent commodo cursus magna, vel
+				scelerisque nisl consectetur et. Cras mattis consectetur purus sit amet fermentum. Cras
+				justo odio, dapibus ac facilisis in, egestas eget quam. Integer posuere erat a ante
+				venenatis dapibus posuere velit aliquet. Morbi leo risus, porta ac consectetur ac,
+				vestibulum at eros. Nullam id dolor id nibh ultricies vehicula ut id elit. Sed posuere
+				consectetur est at lobortis. Vestibulum id ligula porta felis euismod semper. Maecenas
+				faucibus mollis interdum. Cum sociis natoque penatibus et magnis dis parturient montes,
+				nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis
+				vestibulum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Fusce dapibus,
+				tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet
+				risus.`),x.forEach(a),C.forEach(a),v.forEach(a),p.forEach(a),this.h()},h(){d(i,"class","text-info"),d(u,"class","col-12 col-md-3"),d(r,"class","col-12 col-md-6"),d(e,"class","row py-4 mb-3"),d(t,"class","container")},m(m,p){S(m,t,p),s(t,e),s(e,u),s(u,i),s(i,b),s(e,f),s(e,r),s(r,l),s(l,h)},p:q,i:q,o:q,d(m){m&&a(t)}}}class k extends V{constructor(t){super();j(this,t,null,y,A,{})}}export{k as default};
